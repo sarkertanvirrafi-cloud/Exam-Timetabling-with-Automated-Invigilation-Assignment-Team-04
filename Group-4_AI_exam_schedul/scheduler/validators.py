6e1707707dbf4bd87f601faa1cf84cb9eb5f95b1
Sujoy Saha <ssaha2330491@bscse.uiu.ac.bd>
2026-06-13 22:01:48 +0600

    Update validators.py

--- Group-4_AI_exam_schedul/scheduler/validators.py
+++ Group-4_AI_exam_schedul/scheduler/validators.py
@@ -1,3 +1,9 @@
 from __future__ import annotations
 from itertools import combinations
 import pandas as pd
+
+def validate_all(
+    timetable: pd.DataFrame,
+    room_allocations: pd.DataFrame,
+    invigilation_roster: pd.DataFrame,
+    dfs: dict[str, pd.DataFrame],
