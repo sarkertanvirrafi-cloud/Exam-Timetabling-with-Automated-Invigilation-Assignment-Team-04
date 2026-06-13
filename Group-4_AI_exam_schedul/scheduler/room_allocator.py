a78b233af8f33cd789c7537342dace06fc6e97c2
BijoyaSaha <bijoyasaha024@gmail.com>
2026-06-13 22:08:39 +0600

    Update room_allocator.py

--- Group-4_AI_exam_schedul/scheduler/room_allocator.py
+++ Group-4_AI_exam_schedul/scheduler/room_allocator.py
@@ -1,3 +1,9 @@
 from __future__ import annotations
 import pandas as pd
 
+def allocate_rooms(
+    schedule: dict[str, str],
+    courses: pd.DataFrame,
+    enrollments: pd.DataFrame,
+    rooms: pd.DataFrame,
+) 
