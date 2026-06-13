271e141310334b8c98dbfdd2f5d29b96ca2dde58
BijoyaSaha <bijoyasaha024@gmail.com>
2026-06-13 20:39:08 +0600

    Update invigilator_csp.py

--- Group-4_AI_exam_schedul/scheduler/invigilator_csp.py
+++ Group-4_AI_exam_schedul/scheduler/invigilator_csp.py
@@ -4,3 +4,10 @@ import pandas as pd
 
 class InvigilationError(Exception):
     pass
+def assign_invigilators(
+    room_allocations: pd.DataFrame,
+    teachers: pd.DataFrame,
+    course_teachers: pd.DataFrame,
+    teacher_availability: pd.DataFrame,
+    invigilation_rules: pd.DataFrame,
+) -> tuple[pd.DataFrame, pd.DataFrame]:
